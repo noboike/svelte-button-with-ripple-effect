137f94ae54f31abdc864f052ea8280ec03bc1db5
noboike <yazeedalhimali@gmail.com>
2024-07-19 00:46:58 +0300

    update

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-full h-svh flex items-center justify-center">
   <div class="scale-150 flex items-center justify-center gap-5 flex">
-    <Button disabled>Button</Button>
+    <Button>Button</Button>
     <Button class="w-24 h-24" />
   </div>
 </div>
